f9e07fe1a73ff700c2c8505b68726d7d86ba2769
thomashendry <t.hendry@griffith.edu.au>
2021-12-24 16:15:31 +1030

    #3 updated config.ini and relevance checker

--- litfinder.py
+++ litfinder.py
@@ -4,7 +4,7 @@
 Created on Thu Dec  9 11:13:49 2021
 
 @author: thomas hendry 2021
-litfinder program
+LitFinder program
 www.tmhendry.com
 """
 
@@ -16,7 +16,8 @@ def title1():
     print("4: ABDC Quality Check")
     print("5: Relevance Checker")
     print("6: Compile refer library")
-    print("7: Exit")
+    print("7: Open Search file")
+    print("8: Exit")
     
     option1 = int(input("Option: "))
     
@@ -28,20 +29,44 @@ def title1():
         scopus_search()
     elif option1 == 4:
         abdccheck()
+    elif option1 == 5:
+        relevance()
+    elif option1 == 6:
+        refer()
     elif option1 == 7:
+        import os
+        os.system("xdg-open df.csv")
+        title1()
+    elif option1 == 8:
         quit()
         
 def csvcreate():
+    
+    config_proceed = str(input("Update your Google API key and Search engine ID (y/n)?: "))
+    if config_proceed == 'y':    
+    
+        import configparser
+        config = configparser.ConfigParser()
+        print("To use the Googe search function you will need a Google Developer API key",
+              "and a programmable search engine ID. Visit www.developers.google.com for information.")
+        api_google = str(input("Enter your Google Developer API key: "))
+        searchid = str(input("Enter your Google Search Engine ID: "))
+        config['SETTINGS'] = {'API': api_google,
+                              'SEARCHID': searchid}
+
+        with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+                           
     import csv
-    print("By proceeding, a new csv file is created to store your results.")
-    proceed1 = str(input('Proceed (y/n)?: '))
+    
+    proceed1 = str(input('Create a new df.csv to store your results (y/n)?: '))
     if proceed1 == 'y':
         with open('df.csv', 'w') as csvfile:
                 filewriter = csv.writer(csvfile, delimiter=',',
                                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                filewriter.writerow(['Program', 'Query', 'Title', 'Author',
-                                     'Year', 'Volume', 'Issue', 'Page',
-                                     'Journal', 'Snippet', 'URL'])
+                filewriter.writerow(['Program','Date','Query','Title','Author',
+                                     'Year','Volume','Issue','Page',
+                                     'Journal','URL','Snippet','Relevant'])
         print('Okay, a new csv file (df.csv) has been created!')
         title1()
     if proceed1 == 'n':
@@ -51,151 +76,219 @@ def csvcreate():
         title1()
         
 def googlesearch1():
-    
-    def googlesearch(QUERY,QUERY_NUM,YEARS,FILETYPE,LOCATION,PAGE):
+    def googlesearch(QUERY,YEARS,FILETYPE,LOCATION,PAGE):
         import requests
-        from csv import writer
+        import pandas as pd
+        import datetime
+        pd.options.mode.chained_assignment = None  # default='warn'
     
         start = (PAGE - 1) * 10 + 1
-    
         url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={QUERY}&start={start}&dateRestrict=y[{YEARS}]&fileType={FILETYPE}&gl={LOCATION}"
-
         data = requests.get(url).json()
-
-        # get the result items
+        df = pd.read_csv("df.csv")
+        df_google = []
         search_items = data.get("items")
     
-        # iterate over 10 results found
         for i, search_item in enumerate(search_items, start=1):
-        
             title = search_item.get("title")
-            snippet = search_item.get("snippet")
             link = search_item.get("link")
-            print("="*10, f"Result #{i+start-1}", "="*10)
-            print("Title:", title)
-            print("Description:", snippet)
-            print("URL:", link, "\n")
-            
-            r = requests.get(link, timeout=300000)
-            
-            with open(str(QUERY_NUM)+"_"+str(title),'wb') as f:
-                f.write(r.content)
-                
-            list=['Google',QUERY,title,'-','-',snippet, link]
-            
-            with open('df.csv','a',newline='') as f_object:
-                writer_object = writer(f_object)
-                writer_object.writerow(list)
-                f_object.close()
-
-        print("These results are now saved on your computer!")
-
-        CONT = str(input("Continue search (y/n)?: "))
-
+            snippet = search_item.get("snippet")
+            df_google.append(
+                {
+                    'Program': 'Google',
+                    'Date': datetime.datetime.now(),
+                    'Query': QUERY,
+                    'Title': title,
+                    'Snippet': snippet,
+                    'URL': link
+                    }
+                )
+        pd.DataFrame(df_google)
+        df = df.append(df_google, ignore_index = True)
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+        df.to_csv('df.csv')
+        print("Page",PAGE,"saved in df.csv.")
+        CONT = str(input("Continue to next page (y/n)?: "))
+    
         if CONT == 'y':
             PAGE = PAGE + 1
-            googlesearch(QUERY,QUERY_NUM,YEARS,FILETYPE,LOCATION,PAGE)
-    
+            googlesearch(QUERY,YEARS,FILETYPE,LOCATION,PAGE)
+        
         elif CONT == 'n':
             print("Finished")
             print("")
             title1()
- 
-    API_KEY = "API KEY HERE"
-    SEARCH_ENGINE_ID = "SEARCH ENGINE ID"
-    print("Google Downloader searches Google (not Google Scholar) for files according to a specified query.",
-          "These files are saved locally. The csv file is also updated with file information.")
+    
+    import configparser
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    
+    API_KEY = config['SETTINGS']['API']
+    SEARCH_ENGINE_ID = config['SETTINGS']['SEARCHID']
+    print("Search Google (not Google Scholar) for files according to a specified query.",
+          "The csv file is updated with file information.")
     QUERY = str(input("Type your query: "))
-    QUERY_NUM = int(input("Enter a query number for your reference: "))
     YEARS = int(input("How many years back would you like to search?: "))
     FILETYPE = str(input("What file type would you like (i.e. pdf, doc etc.)?: "))
-    LOCATION = str(input("What location would you like to search from (i.e. au, us etc.)?: "))
+    LOCATION = str(input("What location would you like to search from (i.e. au, us etc.)?: "))  
     PAGE = int(input("What page from the search?: "))
-
-    googlesearch(QUERY,QUERY_NUM,YEARS,FILETYPE,LOCATION,PAGE)
-
-def scopus_search():
+    googlesearch(QUERY,YEARS,FILETYPE,LOCATION,PAGE)
     
+def scopus_search():
     from pybliometrics.scopus import ScopusSearch
     import pandas as pd
-    import os
-    print("Scopus Search searches Scopus for files according to a specified query.",
-          "The csv file is updated with file information. ",
-          "Currently, pdf versions are not saved locally.",
+    import datetime
+    
+    print("Search Scopus for papers according to a specified query.",
+          "The csv file is updated with file information.",
           "Refer to Scopus for field codes.")
+    
     QUERY_SCOPUS = str(input("Type your query: "))
     
     s = ScopusSearch(QUERY_SCOPUS, subscriber=True)
     
-    df = pd.DataFrame(pd.DataFrame(s.results))
-    df['Program'] = 'Scopus'
-    df['Query'] = QUERY_SCOPUS
-    df = df[['Program','Query','title','author_names','coverDate',
+    df_scopus = pd.DataFrame(pd.DataFrame(s.results))
+    df_scopus['Program'] = 'Scopus'
+    df_scopus['Date'] = datetime.datetime.now()
+    df_scopus['Query'] = QUERY_SCOPUS
+    df_scopus = df_scopus[['Program','Date','Query','title','author_names','coverDate',
              'volume','issueIdentifier','pageRange','publicationName',
              'description','doi']]
-    df['doi'] = str('https://www.doi.org/')+df['doi'] 
-    df.rename(columns = {'title':'Title','author_names':'Author','coverDate':'Year',
+    df_scopus['doi'] = str('https://www.doi.org/')+df_scopus['doi'] 
+    df_scopus.rename(columns = {'title':'Title','author_names':'Author','coverDate':'Year',
                          'volume':'Volume','issueIdentifier':'Issue','pageRange':'Page',
                          'publicationName':'Journal','description':'Snippet',
                          'doi':'URL'},inplace = True)
-    df['Year'] = df['Year'].astype(str).str[:4]
+    df_scopus['Year'] = df_scopus['Year'].astype(str).str[:4]
     
-    print('The search found',len(df.Year),'results. The file df.csv has been updated.')
-    df.to_csv('df_scopus.csv')
-
-    df1 = pd.read_csv("df.csv")
-    df2 = pd.read_csv("df_scopus.csv")
-    pd.concat([df1, df2]).to_csv('df.csv', index=False)
-    os.remove("df_scopus.csv")
+    print('The search found',len(df_scopus.Year),'results. The df.csv file has been updated.')
+    
+    df = pd.read_csv("df.csv")
+    df = df.append(df_scopus, ignore_index = True)
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df.to_csv('df.csv')
     title1()
 
-#%%
 def abdcreduce():
     import pandas as pd
     journals = pd.read_csv('journals.csv')
     journals = journals[['Title', 'Rating']]
     journals.rename(columns = {'Title':'Journal'},inplace = True)
-    results = pd.read_csv('df_abdc.csv')
-    results2 = pd.merge(results, journals)
-    df = results2[['Program','Query','Title','Author','Year','Volume',
-                   'Issue','Page','Journal','Snippet','URL','Rating']]
+    results = pd.read_csv('df.csv')
     
-    print("You can now remove papers according to the rating of their corresponding journal.",
-          "Type c to remove C and below, type b to remove B and below etc.")
-    cutoff = str(input("What rating would you like to remove (a, b or c)?: "))
+    if 'Rating' in results.columns:
+        del results['Rating']
     
-    if cutoff == 'keep all':
-        df.to_csv("df_abdc_all.csv")
-    elif cutoff == 'c':
-        dfabovec = df.loc[df.Rating != 'C']
-        dfabovec.to_csv("df_abdc_noC")
-    elif cutoff == 'b':
-        dfaboveb = df.loc[(df.Rating != 'C') & (df.Rating != 'B')]
-        dfaboveb.to_csv("df_abdc_noBC")
-    elif cutoff == 'a':
-        dfabovea = df.loc[(df.Rating != 'C') & (df.Rating != 'B') & (df.Rating != 'A')]
-        dfabovea.to_csv("df_abdc_noABC")
-
-    print("A new csv file has been created based on the ratings you specified.")
+    results2 = pd.merge(results, journals, on = ['Journal'], how = 'outer')
+    df = results2[['Program','Date','Query','Title','Author','Year','Volume',
+                   'Issue','Page','Journal','URL','Snippet','Rating','Relevant'
+                   ]]
+    df.dropna(subset = ['Program'], inplace = True)
+    df = df.sort_values(by=['Rating'])   
+    df.Rating = df.Rating.fillna('No rating')
+    df.to_csv("df.csv")
+    print("The csv file has been updated with ABDC ratings.")
     title1()
 
 def abdccheck():
     import pandas as pd
     import numpy as np
-    print("This will remove all papers printed in journals not rated by ABDC.")
     journals = pd.read_csv("journals.csv")
     searches = pd.read_csv("df.csv")
     print("Your searches have resulted in", len(searches), "papers.")
-    searches["Ranked"] = np.where(searches.Journal.isin(journals.Title), True, False)
-    searches2 = searches[searches.Ranked == True]
-    print("The new csv file (df_abdc.csv) will have", len(searches2), "papers.")
-    goahead = str(input("Proceed (y/n)?: "))
+    searches["Rated"] = np.where(searches.Journal.isin(journals.Title), True, False)
+    searches2 = searches[searches.Rated == True]
+    print(len(searches2), "papers are rated by ABDC.")
+    goahead = str(input("Proceed to update csv (y/n)?: "))
     
     if goahead == 'y':
-        searches2.to_csv("df_abdc.csv")
         abdcreduce()
     else:
         title1()
 
+def relevance():
+    import pandas as pd
+    pd.options.mode.chained_assignment = None  # default='warn'
+    df = pd.read_csv('df.csv')
+    
+    dupes = str(input("Before sorting the relevant papers, do you want to delete any duplicates (y/n)?: "))
+    
+    if dupes == 'y':
+        x = len(df.Program)
+        df = df.drop_duplicates(subset=['Title', 'URL'])
+        print(x-len(df.Program),"duplicates were removed.")
+    
+    for i in df.index:
+        if df.Relevant[i] == 'True':
+            df.Relevant[i] = df.Relevant[i]
+        elif df.Relevant[i] == 'False':
+            df.Relevant[i] = df.Relevant[i]
+        else:
+            print("")
+            print('Title:',df.Title[i])
+            print("")
+            print('Snippet:',df.Snippet[i])
+            print("")
+            print('Rating:',df.Rating[i])
+            print("")
+            rel = str(input('Is this paper relevant (k=keep, l=leave out, e=exit)?: '))
+    
+            if rel == 'e':
+                df = df[['Program','Date','Query','Title','Author','Year','Volume',
+                         'Issue','Page','Journal','URL','Snippet','Rating',
+                         'Relevant']]
+                df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+                df.Relevant = df.Relevant.fillna('Not checked')
+                df.to_csv('df.csv')
+                print("The csv file has been updated.")
+                title1()
+        
+            elif rel == 'k':
+                df.Relevant[i] = True
+        
+            elif rel == 'l':
+                df.Relevant[i] = False
+
+def refer():
+    import pandas as pd
+    import os
+    df = pd.read_csv("df.csv")
+    df.Year.fillna(1000,inplace=True)
+    df.fillna('',inplace=True)
+    
+    print("This program will replace any existing refer bibliography file.",
+          "The file may have incomplete data (especially from Google).")
+    
+    proc = str(input("Proceed (y/n)?: "))
+    
+    if os.path.exists('bib.txt') == True:
+        os.remove('bib.txt')
+        
+    if proc == 'y': 
+        
+        limitr = str(input("Limit to relevant papers (y/n)?: "))
+        if limitr == 'y':
+            df = df[df.Relevant == 'True']
+            
+        for i in df.index:
+            x = df.Author.str.split(';', expand=False)
+            for p in x[i]:
+                print('%A',p,file=open("bib.txt", "a"))
+            print('%T',df.Title[i],file=open("bib.txt", "a"))
+            print('%J',df.Journal[i],file=open("bib.txt", "a"))
+            print('%V',str(df.Volume[i]),file=open("bib.txt", "a"))
+            print('%N',df.Issue[i],file=open("bib.txt", "a"))
+            print('%P',df.Page[i],file=open("bib.txt", "a"))
+            print('%D',int(df.Year[i]),file=open("bib.txt", "a"))
+            print("",file=open("bib.txt", "a"))
+        
+        print("A new refer bibliography file (bib.txt) has been created.",
+              "No changes were made to the existing df.csv file")
+
+    else:
+        title1()
+        
+    title1()
+            
 print("Welcome to litfinder. Please select an option below:")
 title1()
